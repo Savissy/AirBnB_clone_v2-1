39228f8e31312e321e4fc8ee23a1734015687643
NatePillay <nathan94pillay@gmail.com>
2023-03-21 14:41:37 -0700

    DB user and password working

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- Create a database
 -- Create hbnb_dev_db if it doesn't exist and manage the previlages of hbnb_dev@localhos
-CREATE IF NOT EXISTS hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
